860b4fbfc9771523c9d278afbb3cd406bb9e3d2b
Uku Reinkubjas <ukureinkubjas@gmail.com>
2024-09-10 14:59:57 +0300

    frontend stuff

---
+++ social-network-frontend/src/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p> &copy; {new Date().getFullYear()} Social-Network by Ureinkub and Kelmik</p>
+        </footer>
+    );
+}
+export default Footer
---
+++ social-network-frontend/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+
+    return (
+        <header>
+            <h1>Social-Network</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+
+}
+
+export default Header
