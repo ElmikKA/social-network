efe9fca13e8b87a297ac2db4d865a616996ebff3
Uku Reinkubjas <ukureinkubjas@gmail.com>
2024-09-17 18:40:38 +0300

    base

--- backend/db/init.go
+++ backend/db/init.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -9,6 +11,17 @@ import (
 func InitDb() (*sql.DB, error) {
 	Db, err := sql.Open("sqlite3", "./db/sn.db")
 	if err != nil {
+		return nil, err
+	}
+
+	triggers, err := os.ReadFile("./db/triggers.sql")
+	if err != nil {
+		fmt.Println("error adding triggers")
+		return nil, err
+	}
+	_, err = Db.Exec(string(triggers))
+	if err != nil {
+		fmt.Println("error adding triggers")
 		return nil, err
 	}
 
